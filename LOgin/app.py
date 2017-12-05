a4992e6cab676f1f35700be6d0c0ef313562f0a3
Vlad-168 <33571295+Vlad-168@users.noreply.github.com>
2017-12-06 00:02:41 +0300

    Add files via upload

--- LOgin/app.py
+++ LOgin/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
+engine = create_engine('sqlite:///tutorial.db', echo=True)
+
  
 app = Flask(__name__)
 
@@ -10,12 +14,25 @@ def home():
         return render_template('login.html')
     else:
         return "Hello !  <a href='/logout'>Logout</a>"
+@app.route('/test')
+def test():
  
+    POST_USERNAME = "python"
+    POST_PASSWORD = "python"
+    POST_SEC_PASSWORD = "python"
+ 
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.login.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]),User.sec_password.in_([POST_SEC_PASSWORD]) )
+    result = query.first()
+    if result:
+        return "Object found"
+    else:
+        return "Object not found " + POST_USERNAME + " " + POST_PASSWORD + " " + POST_SEC_PASSWORD
 @app.route('/login', methods=['POST'])
+
 def do_admin_login():
-    
-    
-    if request.form['password'] == '123' and request.form['username'] == "admin":
+    if POST_USERNAME==request.form['username'] and POST_PASSWORD == request.form['password']:
         session['logged_in'] = True
     else:
         flash('wrong password!')
@@ -35,8 +52,18 @@ def home_1():
  
 @app.route('/reg_login', methods=['POST'])
 def do_admin_login_1():
-    if len(request.form['password']) >= 8 and request.form['login'] != "":
+    POST_USERNAME = str(request.form['login'])
+    POST_PASSWORD = str(request.form['password'])
+    POST_SEC_PASSWORD = str(request.form['sec_password'])
+
+ 
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.login.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]),User.sec_password.in_([POST_SEC_PASSWORD]) )
+    result = query.first()
+    if len(request.form['password']) >= 8 and result and  request.form['login'] != "" and len(request.form['sec_password']) >= 8 and request.form['password'] == request.form['sec_password']:
         session['reg_in'] = True
+
     else:
         flash('wrong password!')
     return home_1()
